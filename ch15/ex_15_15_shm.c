4c61224fca095de7765082a88b354081686d13fb
Monstar <652141046@qq.com>
2017-11-04 21:48:28 +0800

    习题15-15,使用XSI共享存储函数代替共享存储映射区

--- ch15/ex_15_15_shm.c
+++ ch15/ex_15_15_shm.c
@@ -1,82 +1,62 @@
-#include <stdio.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <string.h>
 #include <sys/stat.h>
 #include <sys/shm.h>
+#include "apue.h"
+#include <fcntl.h>
+#include <sys/mman.h>
 #include "synchronous.c"
 
-#define BUF_SIZE 100
+#define	NLOOPS		1000
+#define	SIZE		sizeof(long)	/* size of shared memory area */
 
-static volatile sig_atomic_t sigflag;
-
-int main(int argc, char *argv[])
+static int
+update(long *ptr)
 {
-	FILE *fp;
-	pid_t pid;
-	int count = 0, shmid;
-	char buf[1024] = "";
-	long *lastoffptr;
-	
-	shmid = shmget(IPC_PRIVATE, BUF_SIZE, IPC_CREAT | 0666);
-	
-	if(shmid == -1)
-	{
-		printf("Shared Memory Created error...\n");
-		exit(-127);
-	}
-
-	lastoffptr = shmat(shmid, NULL, 0);
-	
-	if((fp = fopen("tt.txt", "w+")) < 0)	
-	{
-		perror("open error or write error!");
-		exit(-1);
-	}
-	
-	
-	fprintf(fp, "0\ttest\n");
-	fflush(fp);
-	TELL_WAIT();
-	
-	if((pid = fork()) > 0)//parent
-	{
-		while(1){
-			WAIT_CHILD();
-						
-			printf("the current ftell=%ld\n", ftell(fp));
-			
-			fseek(fp, *lastoffptr, SEEK_SET);
-			fscanf(fp, "%d\t%s\n", &count, buf);
-			printf("parent read from the file count=%d.\n", count);
-			
-			*lastoffptr = ftell(fp);
-			fprintf(fp,"%d\t%s\n", count+1, "parent");
-			fflush(fp);
-			
-			TELL_CHILD(pid);
-		}
-	}else //child
-	{
-		while(1){
-			
-			fseek(fp, *lastoffptr, SEEK_SET);
-			fscanf(fp, "%d\t%s\n", &count, buf);
-			printf("child read from the file count=%d.\n", count);
-			
-			*lastoffptr = ftell(fp);
-			if(fprintf(fp,"%d\t%s\n", count+1, "child")<0){printf("child write error\n");}
-			fflush(fp);
-			
-			
-			TELL_PARENT(getppid());
-			WAIT_PARENT();
-		}
-	}
-	
-	fclose(fp);
-	return 0;
+	return((*ptr)++);	/* return value before increment */
 }
 
+
+
+
+
+
+int
+main(void)
+{
+	int		fd, i, counter, shmid;
+	pid_t	pid;
+	void	*area;
+
+
+	if ((fd = open("/dev/zero", O_RDWR)) < 0)
+		err_sys("open error");
+	if ((shmid = shmget(IPC_PRIVATE, BUF_SIZE, IPC_CREAT | 0666)) == -1)
+		err_sys("Shared Memory Created error");
+	close(fd);		/* can close /dev/zero now that it's mapped */
+
+	TELL_WAIT();
+
+	if ((pid = fork()) < 0) {
+		err_sys("fork error");
+	} else if (pid > 0) {			/* parent */
+		for (i = 0; i < NLOOPS; i += 2) {
+			if ((counter = update((long *)area)) != i)
+				err_quit("parent: expected %d, got %d", i, counter);
+            printf("%d\n", counter);
+
+			TELL_CHILD(pid);
+			WAIT_CHILD();
+		}
+	} else {						/* child */
+		for (i = 1; i < NLOOPS + 1; i += 2) {
+			WAIT_PARENT();
+
+			if ((counter = update((long *)area)) != i)
+				err_quit("child: expected %d, got %d", i, counter);
+            printf("%d\n", counter);
+
+			TELL_PARENT(getppid());
+		}
+	}
+
+	exit(0);
+}
