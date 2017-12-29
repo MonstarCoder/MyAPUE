fd0aa3c748077ed62ef66d34b04dbdf1ad4f584e
marvin <652141046@qq.com>
2017-12-29 16:05:41 +0800

    bug fix: linux默认的signal(sigaction实现)不需要对信号处理函数进行重新注册

--- ch10/suspend2.c
+++ ch10/suspend2.c
@@ -1,5 +1,8 @@
-#include "apue.h"
-#include "signal.c"
+/*#include "apue.h"*/
+/*#include "signal."*/
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
 
 volatile sig_atomic_t	quitflag;	/* set nonzero by signal handler */
 
@@ -18,10 +21,10 @@ main(void)
 	sigset_t	newmask, oldmask, zeromask;
     /*while (1) {*/
 	    if (signal(SIGINT, sig_int) == SIG_ERR)
-		    err_sys("signal(SIGINT) error");
+		    printf("signal(SIGINT) error");
     /*}*/
 	if (signal(SIGQUIT, sig_int) == SIG_ERR)
-		err_sys("signal(SIGQUIT) error");
+		printf("signal(SIGQUIT) error");
 
 	sigemptyset(&zeromask);
 	sigemptyset(&newmask);
@@ -31,13 +34,13 @@ main(void)
 	 * Block SIGQUIT and save current signal mask.
 	 */
 	if (sigprocmask(SIG_BLOCK, &newmask, &oldmask) < 0)
-		err_sys("SIG_BLOCK error");
+		printf("SIG_BLOCK error");
 
 	while (quitflag == 0)
     {
         sigsuspend(&zeromask);
 //	    if (signal(SIGINT, sig_int) == SIG_ERR)
-//    		err_sys("signal(SIGINT) error");
+//    		printf("signal(SIGINT) error");
     }
 
 	/*
@@ -49,7 +52,7 @@ main(void)
 	 * Reset signal mask which unblocks SIGQUIT.
 	 */
 	if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0)
-		err_sys("SIG_SETMASK error");
+		printf("SIG_SETMASK error");
 
 	exit(0);
 }
